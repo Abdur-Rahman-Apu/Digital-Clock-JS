034ccea0862f41052b617456866999036a20a689
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-09-01 06:39:24 +0600

    Design modified

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const clockContainer = document.querySelector(".clock-container");
-const p = document.createElement("p");
+
+const p1 = document.createElement("p");
+const p2 = document.createElement("p");
 
 setInterval(() => {
   const date = new Date();
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
-
   let dayOrNight = hours >= 12 ? "PM" : "AM";
+
   hours = hours > 12 ? hours - 12 : hours;
 
   hours = String(hours).padStart(2, "0");
   minutes = String(minutes).padStart(2, "0");
   seconds = String(seconds).padStart(2, "0");
 
-  const result = `${hours}:${minutes}:${seconds} ${dayOrNight}`;
+  const result = `${hours}:${minutes}:${seconds}`;
 
-  p.textContent = result;
+  p1.textContent = result;
 
-  clockContainer.appendChild(p);
+  p2.textContent = `${dayOrNight}`;
+
+  clockContainer.appendChild(p1);
+  clockContainer.appendChild(p2);
 }, 1000);
