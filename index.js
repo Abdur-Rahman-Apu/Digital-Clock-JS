e122b9172b2599909c40375fc2446e655695d686
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-09-01 06:10:16 +0600

    Digital clock font added

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ setInterval(() => {
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
 
+  let dayOrNight = hours >= 12 ? "PM" : "AM";
   hours = hours > 12 ? hours - 12 : hours;
 
   hours = String(hours).padStart(2, "0");
   minutes = String(minutes).padStart(2, "0");
   seconds = String(seconds).padStart(2, "0");
 
-  const result = `${hours}:${minutes}:${seconds}`;
+  const result = `${hours}:${minutes}:${seconds} ${dayOrNight}`;
 
   p.textContent = result;
 
